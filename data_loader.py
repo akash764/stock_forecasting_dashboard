51fb365a89e7cbef3bb6f44e8676508923c0cd44
Akash u <akash1932003@gmail.com>
2025-07-08 21:25:50 +0530

    git commit

--- data_loader.py
+++ data_loader.py
@@ -5,7 +5,7 @@ def load_stock_data(ticker, period="2y"):
     try:
         end = datetime.today()
         start = end - timedelta(days=730)  # 2 years
-        df = web.DataReader(ticker, "av-alpha-vantage", start, end, api_key="YOUR_API_KEY")
+        df = web.DataReader(ticker, "av-alpha-vantage", start, end, api_key="IZB96UGQUMS5G0ZV")
         df.reset_index(inplace=True)
         df.rename(columns={"close": "Close", "date": "Date"}, inplace=True)
         return df
