36697e41715ed4eac01323445ecc2214853fb386
Akash u <akash1932003@gmail.com>
2025-07-08 21:15:00 +0530

    git commit

--- data_loader.py
+++ data_loader.py
@@ -1,3 +1,4 @@
+# data_loader.py
 import yfinance as yf
 import pandas as pd
 
@@ -5,10 +6,9 @@ def load_stock_data(ticker, period="2y"):
     try:
         data = yf.download(ticker, period=period)
         if data.empty:
-            print("⚠️ No data found for this symbol.")
             return None
         data.reset_index(inplace=True)
         return data
     except Exception as e:
-        print(f"⚠️ Failed to fetch stock data: {e}")
+        print(f"Error fetching data: {e}")
         return None
