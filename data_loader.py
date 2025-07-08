793c27a1be17e41e92b733652554312433addc05
Akash u <akash1932003@gmail.com>
2025-07-08 22:01:54 +0530

    git commit

--- data_loader.py
+++ data_loader.py
@@ -2,18 +2,16 @@ import streamlit as st
 import yfinance as yf
 import pandas as pd
 
-def load_stock_data(ticker, period="2y"):
-    st.write(f"Loading data from Yahoo Finance for: {ticker}")
-    
+def load_stock_data(ticker, period="1y"):
     try:
+        print(f"Loading data from Yahoo Finance for: {ticker}")
         data = yf.download(ticker, period=period)
         if data.empty:
-            st.error("❌ Failed to fetch data. Please check the symbol or try again.")
+            print("❌ No data found for ticker.")
             return pd.DataFrame()
-
         data.reset_index(inplace=True)
         return data
-
     except Exception as e:
-        st.error(f"❌ Error fetching data: {e}")
+        print(f"❌ Error fetching data: {e}")
         return pd.DataFrame()
+
